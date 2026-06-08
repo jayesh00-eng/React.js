01841fe8267769bc3e358e7943275a74d6dc2337
jayesh00-eng <jayeshpatiledu@gmail.com>
2026-06-08 11:35:52 +0530

    update project

--- Text-utils/src/components/Textutils.jsx
+++ Text-utils/src/components/Textutils.jsx
@@ -32,6 +32,15 @@ const TextUtils = () => {
         >
           Lowercase
         </button>
+        <button
+            onClick={() => {
+           
+            }}
+            type="button"
+            className="btn btn-success m-1"
+          >
+            Reading Speed <i className="ri-dashboard-3-fill"></i>
+          </button>
 
         <button
           className="btn btn-danger"
